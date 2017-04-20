4cfaa143bc629d663c0f7947c63f5ae3d02fa5e3
Jay R Bolton <jayrbolton@gmail.com>
2017-04-20 10:34:56 -0700

    Initial writeup

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
 // Create a new stream with an optional initial value
 const create = val => {
   const streamData = {val, updaters: []}
-  const streamFunc = val => {
-    if(val !== undefined) {
-      update(streamData, val)
-      return streamFunc
-    } else return streamData.val
+  const streamFunc = function(val) {
+    if(arguments.length === 0) return streamData.val
+    update(streamData, val, Number(new Date()))
+    return streamFunc
   }
   streamFunc.data = streamData // cache for later
   streamFunc.toString = () => `stream(${streamData.val})`
@@ -14,9 +12,9 @@ const create = val => {
 }
 
 // Update stream data and all dependents with a new val
-const update = (streamData, val) => {
+const update = (streamData, val, ts) => {
   streamData.val = val
-  for(const i = 0; i < streamData.updaters.length; ++i) {
+  for(var i = 0; i < streamData.updaters.length; ++i) {
     streamData.updaters[i](val)
   }
 }
@@ -24,14 +22,14 @@ const update = (streamData, val) => {
 // Create a new stream with fn applied to all values within stream
 const map = (fn, stream) => {
   const newS = create()
-  stream.data.updaters.push(newS)
+  stream.data.updaters.push(val => newS(fn(val)))
   return newS
 }
 
 // Merge multiple streams into one, where each event on each streams fires separately in the result stream
 const merge = (streams) => {
   const newS = create()
-  for(const i = 0; i < streams.length; ++i) {
+  for(var i = 0; i < streams.length; ++i) {
     streams[i].data.updaters.push(newS)
   }
   return newS
@@ -43,7 +41,7 @@ const scan = (fn, accum, stream) =>
 
 // Collect values from a stream into an array, and emit that array as soon as n values have been collected
 const buffer = (n, stream) => {
-  const newS = create(initialValue)
+  const newS = create()
   var buff = []
   stream.data.updaters.push(val => {
     buff.push(val)
